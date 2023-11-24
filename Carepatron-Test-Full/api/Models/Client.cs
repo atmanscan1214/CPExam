168becc9de3f6e7ce235fe47d930a3048b9e48cf
atmanscan1214 <108166707+atmanscan1214@users.noreply.github.com>
2023-11-24 19:00:37 +0800

    added files via upload.

---
+++ Carepatron-Test-Full/api/Models/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace api.Models
+{
+    public class Client
+    {
+        public Client() { }
+
+        public Client(string id, string firstName, string lastName, string email, string phoneNumber)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+        [Key]
+        [Required]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage ="First Name is required.")]
+        [StringLength(50,MinimumLength =6)]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "last Name is required.")]
+        [StringLength(50,MinimumLength =6)]
+        public string LastName { get; set; }
+        [Required(ErrorMessage ="Email is required.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Phone Number is required.")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+    }
+}
+
